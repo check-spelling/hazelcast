f170755ce4a138a44a2a5d7af3cdd2b0ec79faab
Josh Soref <jsoref@users.noreply.github.com>
2022-03-06 01:18:03 -0500

    spelling: appearance
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hazelcast/src/test/java/com/hazelcast/jet/impl/deployment/ClientDeployment_StandaloneClusterTest.java
+++ hazelcast/src/test/java/com/hazelcast/jet/impl/deployment/ClientDeployment_StandaloneClusterTest.java
@@ -42,7 +42,7 @@ public class ClientDeployment_StandaloneClusterTest extends JetTestSupport {
     public void when_classAddedUsingUcd_then_visibleToJet() throws Exception {
         URL classUrl = new File(CLASS_DIRECTORY).toURI().toURL();
         URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{classUrl}, null);
-        Class<?> personClz = urlClassLoader.loadClass("com.sample.pojo.person.Person$Appereance");
+        Class<?> personClz = urlClassLoader.loadClass("com.sample.pojo.person.Person$Appearance");
 
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.setClassLoader(urlClassLoader);
